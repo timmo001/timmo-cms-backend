25086c874a3488ab287df0b0b79385f5055b2b66
Aidan Timson <contact@timmo.xyz>
2022-03-15 22:48:04 +0000

    Update

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '0b0d21165660d95e0a3bd707e6f9711b'),
+    secret: env("ADMIN_JWT_SECRET", "0b0d21165660d95e0a3bd707e6f9711b"),
   },
 });
